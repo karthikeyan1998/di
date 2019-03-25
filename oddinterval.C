0604087a264be9333e40221d2f59f6a92f6cd24c
karthikeyan1998 <47741331+karthikeyan1998@users.noreply.github.com>
2019-03-25 16:13:41 +0530

    Create oddinterval.C

---
+++ oddinterval.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int up,low,i,k=0;
+printf("enter the lower limit  ");
+scanf("%d",&low);
+printf("enter the upper limit  ");
+scanf("%d",&up);
+for(i=low;i<=up;i++)
+{
+  if(i%2!=0)
+    printf("\n %d",i);
+}
+
+return 0;
+}
